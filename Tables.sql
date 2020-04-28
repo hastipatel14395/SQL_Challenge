ead887ab08ea808d54c1d0cfee6e65982766dc31
Hasti Patel <60442442+hastipatel14395@users.noreply.github.com>
2020-04-28 16:33:08 -0400

    Tables.sql
    -- NOTE! 
    If you have used non-SQL datatypes in your design, you will have to change these here.

---
+++ Tables.sql
@@ -0,0 +1,41 @@
+CREATE TABLE dept_emp (
+    "emp_no" INT NOT NULL,
+    "dept_no" VARCHAR(10) NOT NULL
+);
+
+CREATE TABLE departments (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE dept_manager (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+ 		
+
+CREATE TABLE "employees" (
+    "emp_no" INT   NOT NULL,
+	" emp_title_id" VARCHAR(10) NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(20)   NOT NULL,
+    "last_name" VARCHAR(20)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+CREATE TABLE "titles" (
+    "title_id" INT   NOT NULL,
+    "title" VARCHAR(20)   NOT NULL
+);
